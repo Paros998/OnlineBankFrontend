092438624de78653fbf255e6a4e9de8d54dae993
Dominik Grudzień <62188218+shanodis@users.noreply.github.com>
2022-01-22 21:13:49 +0100

    Employee details view

--- src/views/EmployeeAuthorisedViews/AdminPages/Employees/EmployeeDetailsPage.tsx
+++ src/views/EmployeeAuthorisedViews/AdminPages/Employees/EmployeeDetailsPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import AuthorisedNavbar from "../../../../components/AuthorisedNavbar/AuthorisedNavbar";
-import SideNavbar from "../../../../components/SideNavbar/SideNavbar";
-import ContainerWithBackgroundImage from "../../../../components/EmployeeContainer/ContainerWithBackgroundImage";
-import {useHistory, useParams} from "react-router-dom";
-import {Button} from "react-bootstrap";
-import EmployeeDetailsCard from "../../../../components/Cards/Employee/EmployeeDetails/EmployeeDetailsCard";
-import EmployeeOrdersCard from "../../../../components/Cards/Employee/EmployeeDetails/EmployeeOrdersCard";
+import AuthorisedNavbar from '../../../../components/AuthorisedNavbar/AuthorisedNavbar';
+import SideNavbar from '../../../../components/SideNavbar/SideNavbar';
+import ContainerWithBackgroundImage from '../../../../components/EmployeeContainer/ContainerWithBackgroundImage';
+import { useHistory, useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import EmployeeDetailsCard from '../../../../components/Cards/Employee/EmployeeDetails/EmployeeDetailsCard';
+import EmployeeOrdersCard from '../../../../components/Cards/Employee/EmployeeDetails/EmployeeOrdersCard';
 
 const EmployeeDetailsPage = () => {
-  const {employeeId,orderId}: { employeeId: string,orderId: string} = useParams();
+  const {employeeId}: { employeeId: string,orderId: string} = useParams();
 
   const history = useHistory();
 
@@ -22,8 +22,8 @@ const EmployeeDetailsPage = () => {
       <SideNavbar/>
       <ContainerWithBackgroundImage className='bg-secondary-light'>
         <div className='d-flex rounded-card-10 bg-secondary w-100 text-dark mh-700px mnh-700px '>
-          <EmployeeDetailsCard className='w-60' employeeId={parseInt(employeeId)}/>
-          <EmployeeOrdersCard className='w-40 me-2' employeeId={parseInt(employeeId)} />
+          <EmployeeDetailsCard className='w-50' employeeId={parseInt(employeeId)}/>
+          <EmployeeOrdersCard className='w-50 me-2' employeeId={parseInt(employeeId)} />
         </div>
         <div className='w-90 pe-4 mnh-200px align-items-center d-flex'>
           <Button
