648fbadbdb4b00b0a536b40184dd201c61e37ade
Paros998 <73112078+Paros998@users.noreply.github.com>
2022-01-30 13:52:32 +0100

    bugfixes

--- src/utils/initAxios.ts
+++ src/utils/initAxios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const initAxios = () => {
-  //axios.defaults.baseURL = 'https://pip-backend-server.herokuapp.com';
-  axios.defaults.baseURL = 'http://localhost:8080';
+  axios.defaults.baseURL = 'https://pip-backend-server.herokuapp.com';
+  //axios.defaults.baseURL = 'http://localhost:8080';
 
   const token = localStorage.getItem('JWT_USER_TOKEN');
   token && (axios.defaults.headers.common['Authorization'] = token);
